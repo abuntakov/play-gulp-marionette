13d3a03ee780cf6e13416a3b1c05b8139d7a6b49
Alexander Buntakov <alexander.buntakov@gmail.com>
2016-07-02 23:19:45 +0300

    added hot replacement plugin

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,12 +4,12 @@ require('babel-polyfill');
 
 const PROD = JSON.parse(process.env.PROD_ENV || '0');
 
-let webpack      = require('webpack');
-let path         = require('path');
-let precss       = require('precss');
+let webpack = require('webpack');
+let path = require('path');
+let precss = require('precss');
 let autoprefixer = require('autoprefixer');
-let assetsPath   = path.resolve(__dirname, './public/dist');
-let contextPath  = path.resolve(__dirname, './frontend');
+let assetsPath = path.resolve(__dirname, '../public/dist');
+let contextPath = path.resolve(__dirname, '../frontend');
 
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
@@ -20,10 +20,12 @@ let ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
 	context: contextPath,
 
-	entry: {
-		'theme': ['bootstrap-loader/extractStyles'],
-		'main': './client'
-	},
+	entry: [
+		'webpack-dev-server/client?http://localhost:3000',
+		'webpack/hot/only-dev-server',
+		'bootstrap-loader/extractStyles',
+		'./client'
+	],
 
 	output: {
 		path: assetsPath,
@@ -37,31 +39,36 @@ module.exports = {
 	module: {
 		loaders: [{
 			test: /\.scss$/,
-			loader: ExtractTextPlugin.extract('style','css!postcss!sass')
-		},{
+			loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
+		}, {
 			test: /\.js$/,
 			include: [contextPath],
 			loader: 'babel?presets[]=es2015'
-		},{
+		}, {
 			test: /\.jsx$/,
 			include: [contextPath],
-			loader: 'babel',
+			loaders: ['react-hot', 'babel'],
 			plugins: ['transform-runtime']
 		}]
 	},
 
-	postcss: function () {
-		return [precss, autoprefixer({ browsers: ['last 2 Chrome versions'] })];
+	postcss: function() {
+		return [precss, autoprefixer({
+			browsers: ['last 2 Chrome versions']
+		})];
 	},
 
 	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
 		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.NoErrorsPlugin(),
-		new ExtractTextPlugin('theme.css', { allChunks: true })
+		new ExtractTextPlugin('theme.css', {
+			allChunks: true
+		})
 	]
 };
 
-if(PROD){
+if (PROD) {
 	module.exports.plugins.push(
 		new webpack.optimize.UglifyJsPlugin({
 			compress: {
